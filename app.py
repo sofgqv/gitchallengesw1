bff1110b6e6cfe18a00fe61e46e960e2c469fa48
Carlos Balbuena <carlosbalbuena@MacBook-Air-de-Carlos.local>
2023-05-20 11:33:02 -0500

    First Commit

---
+++ app.py
@@ -0,0 +1,30 @@
+def main():
+    # do not modify ==>
+    aurora()
+    triplea()
+    botdiscord()
+    gymexplorer()
+    quedijo()
+    signtalk()
+    # <== do not modify
+
+def aurora():
+    print("Este es el equipo Aurora")
+
+def triplea():
+    print("Este es el equipo TripleA")
+
+def botdiscord():
+    print("Este es el equipo Bot Discord")
+
+def gymexplorer():
+    print("Este es el equipo Gym Explorer")
+
+def quedijo():
+    print("Este es el equipo Que Dijo")
+
+def signtalk():
+    print("Este es el equipo Sign Talk")
+
+
+main()
